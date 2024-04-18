adf7df70db954c4ed061b3f30cfb232e4d61498e
Purushotham Maddi <purushothammaddi890@gmail.com>
2024-04-18 12:34:25 +0530

    commit javascript

--- Datatype.js
+++
@@ -1,7 +0,0 @@
-var oneNum =50; //number
-console.log(typeof oneNum);
-// typeof is a special operator in js,It will let you know data type of variable.
-oneNum = "purushotham"; //string
-console.log(typeof oneNum);
-oneNum = true;
-console.log(typeof oneNum);
--- dowhile.js
+++
@@ -1,5 +0,0 @@
-var a=1;
-do{
-    console.log(a) //1 
-    a++;
-}while(a < 11);
